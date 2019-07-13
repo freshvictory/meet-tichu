90ab4a5654f82ceb6a4c3bac4aa5f6bd026c2c5d
freshvictory <justin.renjilian@gmail.com>
2019-07-13 19:58:33 -0400

    Add a whole lot of copy

--- src/Main.elm
+++ src/Main.elm
@@ -27,6 +27,12 @@ type alias Model =
     {}
 
 
+type alias Colors =
+    { red: Color
+    , yellow: Color
+    }
+
+
 init : ( Model, Cmd Msg )
 init =
     ( {}, Cmd.none )
@@ -59,14 +65,14 @@ view model =
 
 banner : Model -> Html Msg
 banner model =
-    div [ css [ backgroundColor (hex "#d42c2c"), padding (px 40) ] ]
-        [ h1 [ css [ color (hex "#ffed00"), fontSize (px 50), margin (px 0), paddingTop (px 250) ] ] [ text "Meet tichu.cards"]
+    div [ css [ backgroundColor colors.red, padding (px 40) ] ]
+        [ h1 [ css [ color colors.yellow, fontSize (px 50), margin (px 0), paddingTop (px 250) ] ] [ text "Meet tichu.cards"]
         , h2 [ css [ marginBottom (px 30), marginTop (px 0) ] ] [ text "THE TICHU SCORING APP" ]
-        , p [ css [ fontSize (px 24), color (hex "#fff"), Css.width (pct 50) ] ] [ text "Players can discover all that tichu.cards has to offer with an ∞-day free trial and access to 100+ games in three easy steps:" ]
+        , p [ css [ fontSize (px 24), color (hex "#fff"), maxWidth (px 500) ] ] [ text "Players can discover all that tichu.cards has to offer with an ∞-day free trial and access to 100+ games in three easy steps:" ]
         , ol [ css [ fontSize (px 20), color (hex "#fff") ] ]
              [ li [ css [ margin (px 10) ] ]
                   [ text "Go to "
-                  , a [ css [ color (hex "#ffed00"), textDecoration none ]
+                  , a [ css [ color colors.yellow, textDecoration none ]
                       , href "https://tichu.cards"
                       , target "_blank" ]
                       [ text "tichu.cards" ]
@@ -74,7 +80,7 @@ banner model =
              , li [ css [ margin (px 10) ] ] [ text "Add the site to your home screen (optional)" ]
              , li [ css [ margin (px 10) ] ] [ text "Start enjoying Tichu" ]
              ]
-        , div [ css [ marginTop (px 20), display block ] ]
+        , div [ css [ marginTop (px 30), display block ] ]
               [ a [ css [ display block, maxWidth maxContent, borderRadius (px 30), fontSize (px 18), padding2 (px 14) (px 40), textDecoration none, color (hex "#bbb"), backgroundColor (hex "#ffed00") ]
                   , href "https://tichu.cards?t=\"A bunch of tracking info to know that you came from meet.tichu.cards so that I can more effectively sell advertisments and collect information on you so I can build a profile of you so that I can more effectively make fun of you when you lose"
                   , target "_blank"
@@ -85,8 +91,34 @@ banner model =
 
 info : Model -> Html Msg
 info model =
-    div [ css [ padding (px 100), fontSize (px 20) ] ]
-        [ p [] [ text "Did you know you can score Tichu games online?" ]
-        , p [] [ text "You can score instantly, for free, using just the device in your hand." ]
-        , p [] [ text "You can even install it as an app for your phone or tablet!" ]
+    div [ css [ padding (px 40), maxWidth (px 740) ] ]
+        [ infoHeader model
+        , infoContent model
         ]
+
+
+infoContent : Model -> Html Msg
+infoContent model =
+    div [ css [ fontSize (px 20) ] ]
+        [ p [ css [ lineHeight (px 28) ] ]
+            [ text "tichu.cards is a K-99 app designed around the modern scoring experience that empowers players and observers with access to the most extensive and diverse scoring configuration available to players. Whether players are scoring for "
+            , a [ href "https://challonge.com/overdrivetichu", target "_blank" ] [ text "tournaments" ]
+            , text " or fun, tichu.cards makes it easy: Powerful scoring tools paired with essential features means players gain 24/7 access to the scores they want and the personalized support they need." 
+            ]
+        , p [ css [ color colors.red ] ]
+            [ text "PLAYERS CAN DISCOVER ALL TICHU.CARDS HAS TO OFFER WITH AN ∞-MONTH TEST DRIVE."
+            ]
+        ]
+
+infoHeader : Model -> Html Msg
+infoHeader model =
+    div []
+        [ div [ css [ fontSize (px 80), fontWeight (int 600), color colors.red ] ] [ text "tichu.cards" ]
+        , div [ css [ fontSize (px 42) ] ] [ text "The right score. At the right time. For every player." ]
+        ]
+
+colors : Colors
+colors =
+    { yellow = (hex "DBB004")
+    , red = (hex "B84444")
+    }
